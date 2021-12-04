2220e75e9db8e9e6bb87018505747dd711b79729
bharti <bhartilokhande79@gmail.com>
2021-12-04 12:04:17 +0530

    cert add

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,19 @@ const admin = require("./routes/admin");
 const cms = require("./routes/cms")
 const express = require('express');
 const cors = require('cors')
-const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+// const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+
+
+
+
 
 const app = express();
 
@@ -10,10 +22,10 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
-app.use(redirectToHTTPS());
+// app.use(redirectToHTTPS());
 
 var corsOptions = {
-  origin: 'https://toonworld.io',
+  origin: '*',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors())
@@ -40,6 +52,12 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`server is running on port ${ PORT }`);
 });
+
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  console.log(`server is running on port ${ PORT }`);
+}).listen(5000);
+
 
 // app.listen(5000, ()=>{
 //     console.log("server is listning on port 5000");
